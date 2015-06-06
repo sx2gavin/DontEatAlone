d1c71df7012082be08b10cd1b173731f700c04a5
Joyce Mac <yt3liu@uwaterloo.ca>
2015-06-06 16:40:09 -0400

    Added basic UI in preference page

--- app/src/main/java/com/example/gavinluo/donteatalone/PreferenceActivity.java
+++ app/src/main/java/com/example/gavinluo/donteatalone/PreferenceActivity.java
@@ -4,14 +4,44 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 
 public class PreferenceActivity extends ActionBarActivity {
+
+    // References to UI components created in activity_preference.xml
+    Spinner _ageSpinner;
+    Spinner _foodSpinner;
+    EditText _distanceEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preference);
+
+        // set the references
+        _ageSpinner = (Spinner)findViewById(R.id.pref_age_spinner);
+        _foodSpinner = (Spinner)findViewById(R.id.pref_food_spinner);
+        _distanceEdit = (EditText)findViewById(R.id.pref_distance);
+
+        initSpinnerItems(_ageSpinner, R.array.pref_age_array);
+        initSpinnerItems(_foodSpinner, R.array.pref_food_array);
+    }
+
+    // Reference: http://developer.android.com/guide/topics/ui/controls/spinner.html
+    private void initSpinnerItems(Spinner spinner, int arrayNum){
+        // Create an ArrayAdapter using hte string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                arrayNum, android.R.layout.simple_spinner_item);
+
+        // Specificy the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 
     @Override
