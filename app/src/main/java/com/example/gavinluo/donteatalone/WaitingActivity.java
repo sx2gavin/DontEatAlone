34293f0e1128a28e3657409282ded81e7c534f43
AAAAAAngela <Jing@v1020-wn-42-173.campus-dynamic.uwaterloo.ca>
2015-06-14 17:41:31 -0400

    updated waiting page

---
+++ app/src/main/java/com/example/gavinluo/donteatalone/WaitingActivity.java
@@ -0,0 +1,45 @@
+package com.example.gavinluo.donteatalone;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class WaitingActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_waiting);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_waiting, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void cancelSearching() {
+        // TODO: remove user in the searching table
+        //finish();
+
+    }
+}
