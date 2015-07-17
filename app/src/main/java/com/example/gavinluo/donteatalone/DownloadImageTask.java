9791d4798f112aa98b9495efd4ad751fb127bb99
Joyce Mac <yt3liu@uwaterloo.ca>
2015-07-17 17:45:30 -0400

    store start time and end time as timestamps in preference page

--- app/src/main/java/com/example/gavinluo/donteatalone/DownloadImageTask.java
+++ app/src/main/java/com/example/gavinluo/donteatalone/DownloadImageTask.java
@@ -9,6 +9,8 @@ import android.widget.ImageView;
 import java.io.InputStream;
 
 /**
+ * Source: http://stackoverflow.com/questions/2471935/how-to-load-an-imageview-by-url-in-android
+ *
  * Created by joyce on 2015-07-17.
  */
 public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
