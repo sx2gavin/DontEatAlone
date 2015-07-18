5cb0fccf9e8b1c71396c196a1f12b2109b5dc74c
Gavin <luoxiangyu528@gmail.com>
2015-07-18 14:33:39 -0400

    Added fields in restaurant.java.

--- app/src/main/java/com/example/gavinluo/donteatalone/Restaurant.java
+++ app/src/main/java/com/example/gavinluo/donteatalone/Restaurant.java
@@ -4,4 +4,26 @@ package com.example.gavinluo.donteatalone;
  * Created by GavinLuo on 15-07-17.
  */
 public class Restaurant {
+	public String m_name;
+	public String m_image_url;
+	public int m_rating;
+	public String m_formatted_address;
+	public String m_formatted_phone_number;
+	public float m_latitude;
+	public float m_longtitude;
+	public String m_url;
+	public String m_website;
+
+	public Restaurant(String name, String image_url, int rating, String formatted_address, String formatted_phone_number, float latitude, float longtitude, String url, String website) 
+	{
+		m_name = name;
+		m_image_url = image_url;
+		m_rating = rating;
+		m_formatted_address = formatted_address;
+		m_formatted_phone_number = formatted_phone_number;
+		m_latitude = latitude;
+		m_longtitude = longtitude;
+		m_url = url;
+		m_website = website;
+	}
 }
